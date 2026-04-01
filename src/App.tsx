d1b598b1090e2e29596c67b08dd489a13f9fb151
Haikal Mumtaz <hmumtaz70@gmail.com>
2026-04-01 09:46:11 +0700

    refactor: simplify scroll restoration logic and adjust ProjectsHero padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useLayoutEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { ReactLenis } from 'lenis/react';
+import { ReactLenis, useLenis } from 'lenis/react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,21 +9,23 @@ import Background from './components/Background';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
+  const lenis = useLenis();
 
-  useLayoutEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
-
-function AppContent() {
   useLayoutEffect(() => {
     if ('scrollRestoration' in history) {
       history.scrollRestoration = 'manual';
     }
   }, []);
 
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+    lenis?.scrollTo(0, { immediate: true });
+  }, [pathname, lenis]);
+
+  return null;
+}
+
+function AppContent() {
   return (
     <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
       <Background />
